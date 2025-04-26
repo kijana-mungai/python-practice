871f5ca08f5adbacfdbe23a46712fcbb4742d3a5
David Njagi <njaginmungai@gmail.com>
2025-04-26 20:10:49 +0300

    Addition Function

---
+++ adder.py
@@ -0,0 +1,8 @@
+mynums=[1,2,3,4,5] # list for testing
+def adder(mylist):
+  total=0 # Variable holding total value
+  for i in mylist: # Loop through the list
+    total+=i # Add the current element to the result
+  print(total) # Display the total
+
+adder(mynums) # Call the function
